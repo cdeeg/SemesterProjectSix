fe0fed93e55289f4dbb6953bebef2614200c5643
Carolin Deeg <carolin.deeg@gmail.com>
2016-05-11 17:21:56 +0200

    all the files

---
+++ Source/SemesterProjectSix/SemesterProjectSixGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "SemesterProjectSixGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASemesterProjectSixGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ASemesterProjectSixGameMode();
+};
+
+
+
